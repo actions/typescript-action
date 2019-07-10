a3a044de7282bb4822966d5e0c7a2b8376f314db
Danny McCormick <damccorm@microsoft.com>
2019-07-10 11:02:50 -0400

    Set failure

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import * as core from '@actions/core';
 
 async function run() {
-  const myInput = core.getInput('myInput');
-  core.debug(`Hello ${myInput}`)
+  try {
+    const myInput = core.getInput('myInput');
+    core.debug(`Hello ${myInput}`);
+  } catch (error) {
+    core.setFailed(error.message);
+  }
 }
 
 run();
