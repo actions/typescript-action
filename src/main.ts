ac267491037aaa48a36a3eb9e8b87e0b27a26a72
Tom Heyes <theyes@thoughtworks.com>
2019-10-04 18:27:07 +0200

    wip

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,55 @@
 import * as core from '@actions/core';
-import {wait} from './wait'
+import {inspect} from "util";
+import colors = module
+
+const github = require('@actions/github');
 
 async function run() {
   try {
-    const ms = core.getInput('milliseconds');
+      const ms = core.getInput('autoMergeUser');
     console.log(`Waiting ${ms} milliseconds ...`)
 
-    core.debug((new Date()).toTimeString())
-    await wait(parseInt(ms));
-    core.debug((new Date()).toTimeString())
+      const actionPayload = github.context.payload;
+
+      const testRun = actionPayload.check_run;
+      const testRunStatus = testRun.status;
+      const testRunConclusion = testRun.conclusion;
+
+
+      // if the test run has completed successfully
+      if (testRunStatus === 'completed' && testRunConclusion === 'success') {
+
+          const testRunPullRequest = testRun.pull_request[0];
+
+          const pullRequestNumber = testRunPullRequest.number;
+          const pullRequestSource = testRunPullRequest.head.ref;
+          const pullRequestDestination = testRunPullRequest.base.ref;
+
+          const repoInfo = actionPayload.repository;
+          const repoOwner = repoInfo.owner.login;
+          const repoName = repoInfo.name;
+
+
+
+          // make api call for more PR info
+
+          console.log('Making api call for PR: ' + pullRequestNumber + " " + pullRequestSource + ":" + pullRequestDestination);
+
+          const myToken = core.getInput('myToken');
+          const octokit = new github.GitHub(myToken);
+          const {data: pullRequest} = await octokit.pulls.get({
+              repo: repoName,
+              owner: repoOwner,
+              pull_number: pullRequestNumber
+          });
+
+          console.log('Found PR: ' + pullRequest.title);
+          console.log('PR raised by: ' + pullRequest.user.login)
+
+
+      }
+
+
 
     core.setOutput('time', new Date().toTimeString());
   } catch (error) {
