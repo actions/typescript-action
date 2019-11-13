e109417d869b3d00034e3c13491e73053c3efcd2
Yamboy1 <yamboyd1@gmail.com>
2019-11-14 06:55:53 +1300

    Update typo in src

--- src/wait.ts
+++ src/wait.ts
@@ -1,7 +1,7 @@
 export function wait(milliseconds: number) {
     return new Promise((resolve) => {
         if (isNaN(milliseconds)) { 
-            throw new Error('milleseconds not a number'); 
+            throw new Error('milliseconds not a number'); 
         }
 
         setTimeout(() => resolve("done!"), milliseconds)
