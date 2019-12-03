f9e476406b09c1ed404fb1ba56fda91d302571ec
Ross Brodbeck <hross@github.com>
2019-12-03 11:11:38 -0500

    Update default project to follow conventions

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,19 @@
-import * as core from '@actions/core';
+import * as core from '@actions/core'
 import {wait} from './wait'
 
-async function run() {
+async function run(): Promise<void> {
   try {
-    const ms = core.getInput('milliseconds');
-    console.log(`Waiting ${ms} milliseconds ...`)
+    const ms: string = core.getInput('milliseconds')
+    core.debug(`Waiting ${ms} milliseconds ...`)
 
-    core.debug((new Date()).toTimeString())
-    await wait(parseInt(ms, 10));
-    core.debug((new Date()).toTimeString())
+    core.debug(new Date().toTimeString())
+    await wait(parseInt(ms, 10))
+    core.debug(new Date().toTimeString())
 
-    core.setOutput('time', new Date().toTimeString());
+    core.setOutput('time', new Date().toTimeString())
   } catch (error) {
-    core.setFailed(error.message);
+    core.setFailed(error.message)
   }
 }
 
-run();
+run()
--- src/wait.ts
+++ src/wait.ts
@@ -1,10 +1,9 @@
-export function wait(milliseconds: number) {
-    return new Promise((resolve) => {
-        if (isNaN(milliseconds)) { 
-            throw new Error('milliseconds not a number'); 
-        }
+export async function wait(milliseconds: number): Promise<string> {
+  return new Promise(resolve => {
+    if (isNaN(milliseconds)) {
+      throw new Error('milliseconds not a number')
+    }
 
-        setTimeout(() => resolve("done!"), milliseconds)
-    });
+    setTimeout(() => resolve('done!'), milliseconds)
+  })
 }
-  
