e020744714d32cbec6c705068cc944cebe64cded
Billy Vong <billy@sentry.io>
2020-07-03 07:18:01 -0700

    meta: Add aliases to jest

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,14 @@
 module.exports = {
   clearMocks: true,
   moduleFileExtensions: ['js', 'ts'],
+  moduleNameMapper: {
+    '^@app/(.*)$': '<rootDir>/src/$1',
+    '^@test/(.*)$': '<rootDir>/test/$1',
+  },
+  testEnvironment: 'node',
   testMatch: ['**/*.test.ts'],
   transform: {
-    '^.+\\.ts$': 'ts-jest'
+    '^.+\\.ts$': 'ts-jest',
   },
-  verbose: true
-}
+  verbose: true,
+};
