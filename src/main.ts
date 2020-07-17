0c5a9d364ec19e80b6404f1ec31a672e86d86f95
Ross Brodbeck <hross@github.com>
2020-07-17 14:31:44 -0400

    Fixup dependencies and some other things

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import {wait} from './wait'
 async function run(): Promise<void> {
   try {
     const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`)
+    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
 
     core.debug(new Date().toTimeString())
     await wait(parseInt(ms, 10))
