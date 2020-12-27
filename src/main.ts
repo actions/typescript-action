fe7333be1ae7d144e1a853e875f2351d731c7706
alexshe <alexshe@wix.com>
2020-12-27 12:57:30 +0200

    feat: mkdown native url

--- src/main.ts
+++ src/main.ts
@@ -38,7 +38,7 @@ async function run(): Promise<void> {
       messageContent = readFileSync(fileName, 'utf-8')
     }
     const buildUrl = await getActionUrl()
-    const footer = `\n\n<a href="${buildUrl}" target="_blank">action logs</a>`
+    const footer = `\n---\n[action logs](${buildUrl})`
     const textToPublish = messageContent.concat(footer.toString())
     const parts = repo.split('/')
     const commandUrl = 'POST /repos/:org/:repo/issues/:pull_request_id/comments'
