5079379c676b82e27a15502d8aa2adf751697b20
Kengo TODA <skypencil@gmail.com>
2021-07-17 16:01:51 +0800

    test: bump up jest to v27 that introduces new defaults
    The jest-circus is now default runner, no need to depend on it explicitly.
    The testEnvironment also changed its default to "node".
    Also types/jest is now needless, we can use jest/globals instead.
    https://jestjs.io/blog/2021/05/25/jest-27

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -2,6 +2,7 @@ import {wait} from '../src/wait'
 import * as process from 'process'
 import * as cp from 'child_process'
 import * as path from 'path'
+import {expect, test} from '@jest/globals'
 
 test('throws invalid number', async () => {
   const input = parseInt('foo', 10)
