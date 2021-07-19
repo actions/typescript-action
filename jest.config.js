6182022b1c61d3650f7aab3b36d05314f65dbf0b
Kengo TODA <skypencil+github@gmail.com>
2021-07-19 21:36:00 +0800

    test: bump up jest to v27 that introduces new defaults (#315)
    The jest-circus is now default runner, no need to depend on it explicitly.
    The testEnvironment also changed its default to "node".
    Also types/jest is now needless, we can use jest/globals instead.
    https://jestjs.io/blog/2021/05/25/jest-27

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,7 @@
 module.exports = {
   clearMocks: true,
   moduleFileExtensions: ['js', 'ts'],
-  testEnvironment: 'node',
   testMatch: ['**/*.test.ts'],
-  testRunner: 'jest-circus/runner',
   transform: {
     '^.+\\.ts$': 'ts-jest'
   },
