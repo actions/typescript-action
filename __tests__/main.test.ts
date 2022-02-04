e1ddc95bf56eb93f8477e142a0e074d84bd1469a
Jose Celano <josecelano@gmail.com>
2022-02-04 12:43:36 +0000

    Apply ESLint also to tests

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -1,8 +1,9 @@
-import {wait} from '../src/wait'
-import * as process from 'process'
 import * as cp from 'child_process'
 import * as path from 'path'
+import * as process from 'process'
+
 import {expect, test} from '@jest/globals'
+import {wait} from '../src/wait'
 
 test('throws invalid number', async () => {
   const input = parseInt('foo', 10)
@@ -13,17 +14,18 @@ test('wait 500 ms', async () => {
   const start = new Date()
   await wait(500)
   const end = new Date()
-  var delta = Math.abs(end.getTime() - start.getTime())
+  const delta = Math.abs(end.getTime() - start.getTime())
   expect(delta).toBeGreaterThan(450)
 })
 
 // shows how the runner will run a javascript action with env / stdout protocol
-test('test runs', () => {
-  process.env['INPUT_MILLISECONDS'] = '500'
+test('runs', () => {
+  process.env.INPUT_MILLISECONDS = '500'
   const np = process.execPath
   const ip = path.join(__dirname, '..', 'lib', 'main.js')
   const options: cp.ExecFileSyncOptions = {
     env: process.env
   }
-  console.log(cp.execFileSync(np, [ip], options).toString())
+  const output = cp.execFileSync(np, [ip], options).toString()
+  expect(output).toMatch(/::debug::Waiting 500 milliseconds .../i)
 })
