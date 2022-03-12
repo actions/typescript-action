8b2e39b65896442f43bb9df44f69bc951d5a79cd
Austen Stone <austenstone@github.com>
2022-03-11 20:42:39 -0500

    formatting and @types/node

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -1,8 +1,8 @@
-import {wait} from '../src/wait'
+import { wait } from '../src/wait'
 import * as process from 'process'
 import * as cp from 'child_process'
 import * as path from 'path'
-import {expect, test} from '@jest/globals'
+import { expect, test } from '@jest/globals'
 
 test('throws invalid number', async () => {
   const input = parseInt('foo', 10)
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import * as core from '@actions/core'
-import {wait} from './wait'
+import { wait } from './wait'
 
 async function run(): Promise<void> {
   try {
