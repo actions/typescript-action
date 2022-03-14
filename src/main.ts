ebc7bb069b6ce91a7ce8ddc840139d800f66454d
Austen Stone <austenstone@github.com>
2022-03-14 04:16:22 -0400

    output

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,9 @@ import { wait } from './wait'
 async function run(): Promise<void> {
   try {
     const ms: string = core.getInput('milliseconds')
-    core.info(new Date().toTimeString())
+    core.info(`Start: ${new Date().toTimeString()}`)
     await wait(parseInt(ms, 10))
-    core.info(new Date().toTimeString())
+    core.info(`End: ${new Date().toTimeString()}`)
     core.setOutput('time', new Date().toTimeString())
   } catch (error) {
     core.setFailed(error instanceof Error ? error.message : JSON.stringify(error, null, 2))
