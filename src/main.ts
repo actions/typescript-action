5554e58eac331f14b3d2a439c0fa931479930022
Austen Stone <austenstone@github.com>
2022-03-14 04:00:52 -0400

    cleanup

--- src/main.ts
+++ src/main.ts
@@ -4,15 +4,12 @@ import { wait } from './wait'
 async function run(): Promise<void> {
   try {
     const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
-
-    core.debug(new Date().toTimeString())
+    core.info(new Date().toTimeString())
     await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
-
+    core.info(new Date().toTimeString())
     core.setOutput('time', new Date().toTimeString())
   } catch (error) {
-    if (error instanceof Error) core.setFailed(error.message)
+    core.setFailed(error instanceof Error ? error.message : JSON.stringify(error, null, 2))
   }
 }
 
