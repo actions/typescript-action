db0dbd10477561b8bca91656ae75b0b444f4729d
Daniel Strobusch <1847260+dastrobu@users.noreply.github.com>
2022-04-03 18:20:30 +0200

    generate docs with docc

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,60 @@
 import * as core from '@actions/core'
-import {wait} from './wait'
+import * as exec from '@actions/exec'
+import * as github from '@actions/github'
+import {buildDocs} from './build'
+import { getBooleanInput, getInput } from "@actions/core";
+import {installDocc} from './install'
 
-async function run(): Promise<void> {
+const run = async (): Promise<void> => {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
+    const context = github.context
+    const doccPath = await installDocc()
 
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+    const path: string = core.getInput('path')
+    let fallbackDisplayName = core.getInput('fallback-display-name')
+    if (!fallbackDisplayName) {
+      fallbackDisplayName = context.repo.repo
+    }
 
-    core.setOutput('time', new Date().toTimeString())
+    let fallbackBundleIdentifier = getInput('fallback-bundle-identifier')
+    if (!fallbackBundleIdentifier) {
+      fallbackBundleIdentifier = context.repo.repo
+    }
+
+    const fallbackBundleVersion = getInput('fallback-bundle-version')
+    const additionalSymbolGraphDir = getInput('additional-symbol-graph-dir')
+    const outputPath: string = getInput('output-path')
+
+    let doccHtmlDir = core.getInput('DOCC_HTML_DIR')
+    if (!doccHtmlDir) {
+      doccHtmlDir = `${doccPath}/../share/docc/render`
+    }
+    core.info(`DOCC_HTML_DIR: ${doccHtmlDir}`)
+
+    await buildDocs(
+      'docc',
+      ['convert', path],
+      {
+        '--fallback-display-name': fallbackDisplayName,
+        '--fallback-bundle-identifier': fallbackBundleIdentifier,
+        '--fallback-bundle-version': fallbackBundleVersion,
+        '--additional-symbol-graph-dir': additionalSymbolGraphDir,
+        '--output-path': outputPath
+      },
+      {
+        DOCC_HTML_DIR: doccHtmlDir
+      }
+    )
+
+    const commit: boolean = getBooleanInput('commit')
+    if (commit) {
+      await exec.exec('git', ['add', `${outputPath}/\*`])
+      await exec.exec('commit', ['-m', ''])
+    }
   } catch (error) {
-    if (error instanceof Error) core.setFailed(error.message)
+    if (error instanceof Error) {
+      core.setFailed(error.message)
+    }
   }
 }
 
