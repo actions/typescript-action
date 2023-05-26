727f53228223ac3ee861149ba8f123e2c135c6f7
Andy Kenward <4893048+andykenward@users.noreply.github.com>
2023-05-26 22:30:39 +0000

    fix: eslint & type test files

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -13,17 +13,18 @@ test('wait 500 ms', async () => {
   const start = new Date()
   await wait(500)
   const end = new Date()
-  var delta = Math.abs(end.getTime() - start.getTime())
+  const delta = Math.abs(end.getTime() - start.getTime())
   expect(delta).toBeGreaterThan(450)
 })
 
 // shows how the runner will run a javascript action with env / stdout protocol
-test('test runs', () => {
+test('runs', () => {
   process.env['INPUT_MILLISECONDS'] = '500'
   const np = process.execPath
   const ip = path.join(__dirname, '..', 'lib', 'main.js')
   const options: cp.ExecFileSyncOptions = {
     env: process.env
   }
-  console.log(cp.execFileSync(np, [ip], options).toString())
+  const output = cp.execFileSync(np, [ip], options).toString()
+  expect(output).toMatch(/::debug::Waiting 500 milliseconds .../i)
 })
