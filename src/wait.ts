464b2658f0c5076e4c1b60ad8a2694bca19c1bfd
Nick Alteen <ncalteen@github.com>
2023-08-23 13:39:12 -0400

    Create CI workflow and tests

--- src/wait.ts
+++ src/wait.ts
@@ -1,3 +1,8 @@
+/**
+ * Wait for a number of milliseconds.
+ * @param milliseconds The number of milliseconds to wait.
+ * @returns {Promise<string>} Resolves with 'done!' after the wait is over.
+ */
 export async function wait(milliseconds: number): Promise<string> {
   return new Promise(resolve => {
     if (isNaN(milliseconds)) {
