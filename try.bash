9768d6f7b97f3cd4a7f129e3032cf16e3917fbb3
Justin STYRD Gaming <mammerjam303@gmail.com>
2025-06-13 01:24:46 -0600

    Create try.bash

---
+++ try.bash
@@ -0,0 +1,16 @@
+DevPost Optimization**:
+   ```markdown
+   ## Try it out
+   [Live Demo](https://barrierbreaker.aws.amplifyapp.com)
+   ## Installation
+   ```bash
+   git clone https://github.com/Luckyspot0gold/BarrierBreaker-AI
+   amplify pull --appId d27kxz4w9abcde
+   ```
+   ## AWS Services Used
+   - SageMaker (ML)
+   - Lex (Chatbot)
+   - DynamoDB (NoSQL)
+   - S3 (Storage)
+   - Lambda (Compute)
+   ```
